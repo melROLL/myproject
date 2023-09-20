8882fcb375ffe14037108e06c50a61f055001b28
melROLL <60772441+melROLL@users.noreply.github.com>
2023-09-20 23:02:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask
 
+# Create a Flask web application
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return render_template("index.html")
+# Define a route for the homepage
+@app.route('/')
+def hello_world(): 
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async 
+    src="https://www.googletagmanager.com/gtag/js?id=G-93Q4VXGGMQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || []; 
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+        gtag('config', 'G-QY0W6CSL6X'); 
+    </script>
+      """
+    return prefix_google + "Hello World"
 
-if __name__ == "__main__":
-    app.run()
+def hello_world():
+    return 'Hello World'
 
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
